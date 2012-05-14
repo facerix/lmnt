d9ac121e64df499b3fefee7971b0e365e28333b0
Ryan Corradini <ryan@buyog.com>
2012-05-14 16:24:42 -0400

    added children, and null protection

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ This project exists to supply the missing functionality specified by the W3C's [
 These properties directly correspond to interfaces defined by the [DOM Level 1 Node interface](http://www.w3.org/TR/REC-DOM-Level-1/level-one-core.html#ID-1950641247), with the additional constraint that they operate only nodeType 1 (i.e. Element nodes), ignoring Text, Comment, and other nodeTypes. Nine times out of ten, this is what Web developers want to do anyway, so using these will [simplify your code](http://ejohn.org/blog/element-traversal-api/).
 
 The polyfill also adds the *children* property to the Element prototype, which isn't officially part of the Element Traversal specification, but is implemented in most modern browsers, and adds a conceptual gap left out of the spec: an Element-only version of *childNodes*.
+
+Currently lmnt.js provides its functionality as functions in a window-level namespace (lmnt), but doesn't touch the Element prototype directly, due to the inaccessibility of Element.prototype in the browsers we care the most about polyfilling: Internet Explorer versions 6 and 7. The best way, then, to write code that leans on the polyfill is:
+
+	
+	// alternative 1:
+	var myFirstChild = parentNode.firstElementChild || lmnt.firstElementChild(parentNode);
+	
+	// or, simpler (lmnt calls the native property if it's available):
+	var myFirstChild = lmnt.firstElementChild(parentNode);
+	
+
+There's no minified version yet, but it's coming. I may also provide an alternative script with a true polyfill, if I can figure out how to get IE 6 & 7 to play ball.
--- lmnt.js
+++ lmnt.js
@@ -1,13 +1,13 @@
 //
-// ElementTraversal polyfill
+// LMNT: replicates the Element Traversal interface
 //
 // author: Ryan Corradini
-// version: 1.0
-// date: 10 May 2012
+// version: 1.1
+// date: 14 May 2012
 // license: MIT
 //
 
-// some browsers don't define this constant, which most of the widget bootstraps rely on
+// some browsers don't define this constant, but it makes the code a little easier to read
 window.ELEMENT_NODE = document.ELEMENT_NODE || 1;
 
 /*
@@ -20,19 +20,21 @@ children|childElements
 */
 window.lmnt = function() {
     function _nextElementSibling( el ) {
-        if (el.nextElementSibling) { return el.nextElementSibling; }
+        if ( el && el.nextElementSibling ) { return el.nextElementSibling; }
+        if ( !el ) { return null; }
         do { el = el.nextSibling } while ( el && el.nodeType !== ELEMENT_NODE );
         return el;
     }
 
     function _previousElementSibling( el ) {
-        if (el.previousElementSibling) { return el.previousElementSibling; }
+        if ( el && el.previousElementSibling ) { return el.previousElementSibling; }
+        if ( !el ) { return null; }
         do { el = el.previousSibling } while ( el && el.nodeType !== ELEMENT_NODE );
         return el;
     }
 
     function _firstElementChild( el ) {
-        if (el.firstElementChild) { return el.firstElementChild; }
+        if ( el && el.firstElementChild ) { return el.firstElementChild; }
         el = el ? el.firstChild : null;
         if ( el && el.nodeType == ELEMENT_NODE ) {
             return el;
@@ -42,7 +44,7 @@ window.lmnt = function() {
     }
 
     function _lastElementChild( el ) {
-        if (el.lastElementChild) { return el.lastElementChild; }
+        if ( el && el.lastElementChild ) { return el.lastElementChild; }
         el = el ? el.lastChild : null;
         if ( el && el.nodeType == ELEMENT_NODE ) {
             return el;
@@ -52,10 +54,10 @@ window.lmnt = function() {
     }
 
     function _childElementCount( el ) {
-        if (el.childElementCount) { return el.childElementCount; }
+        if ( el && el.childElementCount ) { return el.childElementCount; }
         if ( el && el.children ) { return el.children.length || 0; }
         var count = 0;
-        el = el.firstChild;
+        el = el ? el.firstChild : null;
         do {
             if ( el && el.nodeType == ELEMENT_NODE ) { count++; }
             el = el.nextSibling;
@@ -63,12 +65,24 @@ window.lmnt = function() {
         return count;
     }
 
+    function _children( el ) {
+        if ( el && el.children ) { return el.children; }
+        var stash = [];
+        el = el ? el.firstChild : null;
+        while ( el ) {
+            if ( el && el.nodeType == ELEMENT_NODE ) { stash.push( el ); }
+            el = el.nextSibling;
+        };
+        return stash;
+    }
+
     return {
         nextElementSibling     : _nextElementSibling,
         previousElementSibling : _previousElementSibling,
         firstElementChild      : _firstElementChild,
         lastElementChild       : _lastElementChild,
-        childElementCount      : _childElementCount
+        childElementCount      : _childElementCount,
+        children               : _children
     }
 }()
 // end of ElementTraversal polyfills
